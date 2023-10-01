856aaaf7a34c83c905d988f6d3a0b5a6e91b3d51
Zachary Honey <zhoney466@gmail.com>
2023-09-30 19:00:04 -0700

    Getting Server set up

--- api/server.js
+++ api/server.js
@@ -1,3 +1,20 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model')
+const server = express();
+server.use(express.json())
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users', (req, res) => {
+    try {
+        const user = User.find()
+        res.status(200).json(user)
+        
+    } catch {
+        res.status(500).json({
+            message: res.message
+        })
+    }
+})
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
